cd965f8a790cb58712cc2bdfef8cbc31ccd31656
shawntsai0312 <shawntsai0312@gmail.com>
2023-11-09 11:30:57 +0800

    fist commit

---
+++ poker.v
@@ -0,0 +1,10 @@
+`timescale 1ns/1ps
+
+module poker(type, i0, i1, i2, i3, i4);
+//DO NOT CHANGE!
+	input  [5:0] i0, i1, i2, i3, i4;
+	output [3:0] type;
+//---------------------------------------------------
+
+
+endmodule
